f4e48abd2b5aee113ccab4111f1407ac981d8de2
2020211001001109-LiuFangbao <100770528+2020211001001109-LiuFangbao@users.noreply.github.com>
2022-04-22 21:27:43 +0800

    week 8

--- src/main/java/com/week5/homework/login.java
+++ src/main/java/com/week5/homework/login.java
@@ -1,27 +1,23 @@
 package com.LiuFangbao.week5.homework;
 
+import com.LiuFangbao.dao.UserDao;
+import com.LiuFangbao.week7.homework.JDBCServletContextListener;
+import com.LiuFangbao.week7.homework.User;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
-import java.util.Objects;
 
 public class login extends HttpServlet {
     Connection con = null;
     @Override
     public void init() throws ServletException {
         super.init();
-        String driver = getServletContext().getInitParameter("driver");
-        String url = getServletContext().getInitParameter("url");
-        String usr = getServletContext().getInitParameter("usr");
-        String pas = getServletContext().getInitParameter("pas");
         try{
-            Class.forName(driver);
-            con = DriverManager.getConnection(url,usr,pas);
+            con = (Connection) getServletContext().getAttribute("con");
             System.out.println("666666");
         }
         catch (Exception e){
@@ -31,58 +27,93 @@ public class login extends HttpServlet {
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        try {
-            String sql = "select * from LiuFangbaotable where username = '"+username+
-                    "' and password = '"+password+"'";
-            ResultSet stm = con.createStatement().executeQuery(sql);
-            if(stm.next()){
-                req.setAttribute("username",stm.getString("username"));
-                req.setAttribute("password",stm.getString("password"));
-                req.setAttribute("email",stm.getString("email"));
-                req.setAttribute("gender",stm.getString("gender"));
-                req.setAttribute("Birthdate",stm.getString("Birthdate"));
-                req.getRequestDispatcher("userinfo.jsp").forward(req,resp);
-            }
-            else {
-                req.setAttribute("message","Username or Password Error");
-                req.getRequestDispatcher("login.jsp").forward(req,resp);
-            }
 
-//            Statement stmt = con.createStatement();
-//            ResultSet selected = stmt.executeQuery("select username,password from LiuFangbaotable");
-//            boolean ave = false;
-//            PrintWriter writer = resp.getWriter();
-//            while(selected.next()){
-//
-//                String use = selected.getString("username");
-//                String pas = selected.getString("password");
-//                System.out.println(username+"  hhhhlou11111  "+use);
-//                System.out.println(password+"  hhhhlou22222  "+pas);
-//                if(Objects.equals(username, use) && Objects.equals(pas, password)){
-//                    writer.println("Congratulations! </br>");
-//                    writer.println("Welcome! " + username +"</br>");
-//                    ave = true;
-//                    writer.println("<a href=\"ConfigDemoServlet\">check</a>");
-//                    break;
-//                }
-//            }
-//            if(!ave){
-//                writer.println("Sorry!!!Login fail!!");
-//            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        System.out.println("nccccb");
+        req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req,resp);
     }
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        UserDao userDao = new UserDao();
+        try {
+            User user = userDao.findByUsernamePassword(con,username,password);
+            if(user != null){
+                String rememberMe=req.getParameter("rememberMe");
+                if(rememberMe!=null&&rememberMe.equals("1")){
+                    Cookie usernameCookie = new Cookie("cUsername",user.getUsername());
+                    Cookie passwordCookie = new Cookie("cPassword",user.getPassword());
+                    Cookie rememberMeCookie = new Cookie("cRememberMe",rememberMe);
+                    usernameCookie.setMaxAge(5);
+                    passwordCookie.setMaxAge(5);
+                    rememberMeCookie.setMaxAge(5);
+                    resp.addCookie(usernameCookie);
+                    resp.addCookie(passwordCookie);
+                    resp.addCookie(rememberMeCookie);
+                }
+                HttpSession session = req.getSession();
+                System.out.println("session id -->" + session.getId());
+                session.setMaxInactiveInterval(10);
+                session.setAttribute("user",user);
+                System.out.println(user.getUsername());
+                req.getRequestDispatcher("WEB-INF/views/userinfo.jsp").forward(req,resp);
+
+            }
+            else {
+                req.setAttribute("message","Username or Password Error");
+                req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req,resp);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//            String sql = "select * from HeZhihaotable where username = '"+username+
+//                    "' and password = '"+password+"'";
+//            ResultSet stm = con.createStatement().executeQuery(sql);
+//            if(stm.next()){
+//                req.setAttribute("username",stm.getString("username"));
+//                req.setAttribute("password",stm.getString("password"));
+//                req.setAttribute("email",stm.getString("email"));
+//                req.setAttribute("gender",stm.getString("gender"));
+//                req.setAttribute("Birthdate",stm.getString("Birthdate"));
+//                req.getRequestDispatcher("userinfo.jsp").forward(req,resp);
+//            }
+//            else {
+//                req.setAttribute("message","Username or Password Error");
+//                req.getRequestDispatcher("login.jsp").forward(req,resp);
+//            }
+//
+////            Statement stmt = con.createStatement();
+////            ResultSet selected = stmt.executeQuery("select username,password from HeZhihaotable");
+////            boolean ave = false;
+////            PrintWriter writer = resp.getWriter();
+////            while(selected.next()){
+////
+////                String use = selected.getString("username");
+////                String pas = selected.getString("password");
+////                System.out.println(username+"  hhhhlou11111  "+use);
+////                System.out.println(password+"  hhhhlou22222  "+pas);
+////                if(Objects.equals(username, use) && Objects.equals(pas, password)){
+////                    writer.println("Congratulations! </br>");
+////                    writer.println("Welcome! " + username +"</br>");
+////                    ave = true;
+////                    writer.println("<a href=\"ConfigDemoServlet\">check</a>");
+////                    break;
+////                }
+////            }
+////            if(!ave){
+////                writer.println("Sorry!!!Login fail!!");
+////            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
