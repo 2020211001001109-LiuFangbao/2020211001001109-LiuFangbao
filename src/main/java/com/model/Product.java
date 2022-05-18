926ec09ebc4d20014ccfa13095f8b2b04e0cc69c
2020211001001109-LiuFangbao <100770528+2020211001001109-LiuFangbao@users.noreply.github.com>
2022-05-18 10:54:25 +0800

    week 11
    week 11

--- src/main/java/com/model/Product.java
+++ src/main/java/com/model/Product.java
@@ -1,27 +1,35 @@
 package com.LiuFangbao.model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
 
-public class Category implements  java.io.Serializable{
+public class Product {
+    private Integer productId;
     private Integer categoryId;
-    private String categoryName;
-    private String description;
-    private Boolean active;
-    public Category(){}
+    private String productName;
+    private String productDescription;
+    private InputStream picture;
+    private Double price;
+    public Product(){
 
-    public Category(String categoryName) {
-        this.categoryName = categoryName;
     }
 
-    public Category(String categoryName, String description, Boolean active) {
-        this.categoryName = categoryName;
-        this.description = description;
-        this.active = active;
+    public Product(Integer productId, Integer categoryId,
+                   String productName, String productDescription,
+                   InputStream picture, Double price) {
+        this.productId = productId;
+        this.categoryId = categoryId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.picture = picture;
+        this.price = price;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
     }
 
     public Integer getCategoryId() {
@@ -32,72 +40,47 @@ public class Category implements  java.io.Serializable{
         this.categoryId = categoryId;
     }
 
-    public String getCategoryName() {
-        return categoryName;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
-    public String getDescription() {
-        return description;
+    public String getProductDescription() {
+        return productDescription;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
     }
 
-    public Boolean getActive() {
-        return active;
+    public InputStream getPicture() {
+        return picture;
     }
 
-    public void setActive(Boolean active) {
-        this.active = active;
+    public void setPicture(InputStream picture) {
+        this.picture = picture;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 
     @Override
     public String toString() {
-        return "Category{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", description='" + description + '\'' +
-                ", active=" + active +
+        return "Product{" +
+                "productId=" + productId +
+                ", categoryId=" + categoryId +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", picture=" + picture +
+                ", price=" + price +
                 '}';
-    }
-
-    public static List<Category> findAllCategory(Connection con){
-        List<Category> list = new ArrayList<Category>();
-        String queryString = "select * from Category";
-        try{
-            PreparedStatement statement = con.prepareStatement(queryString);
-            ResultSet resultSet = statement.executeQuery();
-            while(resultSet.next()){
-                Category c = new Category();
-                c.setCategoryId(resultSet.getInt("categoryid"));
-                c.setCategoryName(resultSet.getString("categoryName"));
-                c.setDescription(resultSet.getString("Description"));
-            }
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    public static String findByCategoryId(Connection con,int categoryId){
-        String categoryName = null;
-        try{
-            String queryString = "select CategoryName from Category where categoryId = ?";
-            PreparedStatement statement = con.prepareStatement(queryString);
-            statement.setInt(1,categoryId);
-            ResultSet resultSet = statement.executeQuery();
-            while(resultSet.next()){
-                categoryName = resultSet.getString("categoryName");
-            }
-        } catch (Exception re){
-            re.printStackTrace();
-        }
-        return categoryName;
     }
 }
